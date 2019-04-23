5ce951d880fd5a0461a79b91bb0e04752587c41f
Manny <manueltrujillo_02@outlook.com>
2019-04-23 00:51:48 -0700

    I got something on the page!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import dog from "./dogs.json";
+import Navbar from "./components/Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    dog,
+    clickDog: [],
+    score: 0
+  };
+  render() {
+    return (
+      <div className="App">
+      <Navbar>
+      score={this.state.score}
+      </Navbar>
+     
+  
+        
+     
+     
+      </div>
+    );
+  }
 }
 
 export default App;
