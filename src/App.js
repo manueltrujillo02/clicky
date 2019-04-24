16e4b03754057580db83cf1bc81b3ecbc8536d24
Manny <manueltrujillo_02@outlook.com>
2019-04-23 19:38:04 -0700

    further along

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component} from 'react';
-import './App.css';
 import dog from "./dogs.json";
 import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper"
-import Card from "./components/Card"
+import Wrapper from "./components/Wrapper";
+import Card from "./components/Card";
+import './App.css';
 
 
 class App extends Component {
   state = {
     dog,
-    clickDog: [],
+    clickedPuppyIds: [],
     score: 0
   };
+
+
+  shuffleScoreCard = id => {
+    let clickedPuppyIds = this.state.clickedPuppyIds;
+
+    if(clickedPuppyIds.includes(id)){
+      this.setState({ clickedPuppyIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+      return;
+    }else{
+      clickedPuppyIds.push(id)
+
+      if(clickedPuppyIds.length === 8){
+        this.setState({score: 8, status: "You Won! Great Job, Smartie! Click to play again!", clickedPuppyIds: []});
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ dog, clickedPuppyIds, score: clickedPuppyIds.length, status: " " });
+
+      for (let i = dog.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [dog[i], dog[j]] = [dog[j], dog[i]];
+      }
+    }
+  }
   render() {
     return (
       <div>
@@ -19,11 +44,16 @@ class App extends Component {
         score={this.state.score}
         </Navbar>
       <Wrapper>
-        {/* {this.state.dogs} */}
-          <Card
-          id={dog.id}
-          image={dog.image}
-          />
+         {this.state.dogs}
+           <Card
+           shuffleScoreCard={this.shuffleScoreCard}
+           id={dog.id}
+           key={dog.id}
+           image={dog.image}
+           />
+
+          
+          
         
       </Wrapper>
   
---
+++ src/components/Score/Score.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Arrow from "react-icons/lib/fa/caret-right";
+
+const Score = props => (
+    <div className="gameScore">
+        <h3 className="score">Your Score<Arrow/>{props.total}</h3>
+        <h3 className="status">{props.status}</h3>
+    
+    </div>
+);
